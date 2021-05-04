67c2ef60de8aa0bfb9f14d17d597ad173896c796
Jan Piotrowski <piotrowski+github@gmail.com>
2021-05-04 23:52:42 +0200

    clean up a bit more

--- server.js
+++ server.js
@@ -2,12 +2,16 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-const { PrismaClient } = require("@prisma/client");
-const prisma = new PrismaClient();
-
 app.get(["/"], async (req, res) => {
+  res.json({ empty: 'result'});
+});
+
+app.get(["/prisma"], async (req, res) => {
+  const { PrismaClient } = require("@prisma/client");
+  const prisma = new PrismaClient();
   const allUsers = await prisma.user.findMany();
+  prisma.$disconnect()
   res.json(allUsers);
 });
 
-app.listen(port, () => console.log(`HelloNode app listening on port ${port}!`));
+app.listen({ address: "0.0.0.0", port: port }, () => console.log(`HelloNode app listening on port ${port}!`));
